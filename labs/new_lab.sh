101490a620739a08d79d6d5d8fe689fa5837a43d
Jeffrey Santos <jmscsedu@gmail.com>
2017-03-29 16:15:01 +0530

    Make changes the new_lab.sh bash script.
    In particular, added details regarding the directory structure of newly
    created labs, as well as modified template file naming to adhere to a
    lowercase naming standard.

--- labs/new_lab.sh
+++ labs/new_lab.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Simple bash script for generating the file structure for a new lab.
 
-# Create the directory:
+# Create the directory structure:
 mkdir "$1"
+mkdir "$1/exemplars"
+mkdir "$1/files"
 
 # Create symbolic links to the `includes` and `graphics` folders:
 ln -s ../includes "$1/includes"
 ln -s ../graphics "$1/graphics"
 
 # Create a copy of the template.tex file with the lab name:
-cp template.tex "$1/LAB_$1.tex"
+cp template.tex "$1/lab_$1.tex"
 
