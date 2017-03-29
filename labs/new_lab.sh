7a3e07af068bc97cdb8b15b40bff5681fb06b273
Jeffrey Santos <jmscsedu@gmail.com>
2017-03-29 15:55:45 +0530

    Create bash script to automate lab creation.

---
+++ labs/new_lab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Simple bash script for generating the file structure for a new lab.
+
+# Create the directory:
+mkdir $1
+
+# Create symbolic links to the `includes` and `graphics` folders:
+ln -s ../includes $1/includes
+ln -s ../graphics $1/graphics
+
+# Create a copy of the template.tex file with the lab name:
+cp template.tex $1/LAB_$1.tex
+
