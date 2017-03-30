9b521746f5331f07f4d67c118d99df2084e5c369
Jeffrey Santos <jmscsedu@gmail.com>
2017-03-31 04:12:11 +0530

    Update lab folder naming scheme.

--- labs/new_lab.sh
+++ labs/new_lab.sh
@@ -2,14 +2,14 @@
 # Simple bash script for generating the file structure for a new lab.
 
 # Create the directory structure:
-mkdir "$1"
-mkdir "$1/exemplars"
-mkdir "$1/files"
+mkdir "lab_$1"
+mkdir "lab_$1/exemplars"
+mkdir "lab_$1/files"
 
 # Create symbolic links to the `includes` and `graphics` folders:
-ln -s ../includes "$1/includes"
-ln -s ../graphics "$1/graphics"
+ln -s ../includes "lab_$1/includes"
+ln -s ../graphics "lab_$1/graphics"
 
 # Create a copy of the template.tex file with the lab name:
-cp template.tex "$1/lab_$1.tex"
+cp template.tex "lab_$1/lab_$1.tex"
 
