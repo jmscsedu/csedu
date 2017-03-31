7486335c3d212ae99303429fb9dc42c918ba652b
Jeffrey Santos <jmscsedu@gmail.com>
2017-03-31 10:04:22 +0530

    Change directory structure to accomodate more templates.

--- new_lab.sh
+++ new_lab.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Simple bash script for generating the file structure for a new lab.
 
+prefix="labs/lab_$1"
+
 # Create the directory structure:
-mkdir "lab_$1"
-mkdir "lab_$1/exemplars"
-mkdir "lab_$1/files"
+
+mkdir $prefix
+mkdir "$prefix/exemplars"
+mkdir "$prefix/files"
 
 # Create symbolic links to the `includes` and `graphics` folders:
-ln -s ../includes "lab_$1/includes"
-ln -s ../graphics "lab_$1/graphics"
+ln -rs templates/includes "$prefix/includes"
+ln -rs templates/graphics "$prefix/graphics"
 
 # Create a copy of the template.tex file with the lab name:
-cp template.tex "lab_$1/lab_$1.tex"
-
+cp templates/lab_template.tex "$prefix/lab_$1.tex"
