83fcb360d18232ad95afbc6c979605e5f80081ac
Jeffrey Santos <jsantos@CID-7621-MB.local>
2019-03-07 09:36:18 +0530

    Add relative symbolic linking support for Mac OS in new_assignment.sh.

--- new_assignment.sh
+++ new_assignment.sh
@@ -10,8 +10,16 @@ mkdir "$prefix/exemplars"
 mkdir "$prefix/files"
 
 # Create symbolic links to the `includes` and `graphics` folders:
-ln -rs templates/includes "$prefix/includes"
-ln -rs templates/graphics "$prefix/graphics"
+
+# Following lines are required for MacOS ('brew install coreutils' for grealpath):
+
+relative=$(grealpath --relative-to="$prefix" templates/)
+ln -s "$relative/includes" "$prefix/includes"
+ln -s "$relative/graphics" "$prefix/graphics"
+
+# Following lines are used for linux:
+# ln -rs templates/includes "$prefix/includes"
+# ln -rs templates/graphics "$prefix/graphics"
 
 # Create a copy of the template.tex file with the assignment name:
 cp templates/assignment_template.tex "$prefix/assignment_$1.tex"
