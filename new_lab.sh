75ff938a624615491c92e944f346b6e95ae126d9
Jeffrey Santos <jmscsedu@gmail.com>
2019-11-22 09:05:10 +0530

    Change new_lab.sh script for Linux use.

--- new_lab.sh
+++ new_lab.sh
@@ -12,13 +12,13 @@ mkdir "$prefix/files"
 
 # Following lines are required for MacOS ('brew install coreutils' for grealpath):
 
-relative=$(grealpath --relative-to="$prefix" templates/)
-ln -s "$relative/includes" "$prefix/includes"
-ln -s "$relative/graphics" "$prefix/graphics"
+# relative=$(grealpath --relative-to="$prefix" templates/)
+# ln -s "$relative/includes" "$prefix/includes"
+# ln -s "$relative/graphics" "$prefix/graphics"
 
 # Following lines are used for linux:
-# ln -rs templates/includes "$prefix/includes"
-# ln -rs templates/graphics "$prefix/graphics"
+ln -rs templates/includes "$prefix/includes"
+ln -rs templates/graphics "$prefix/graphics"
 
 # Create a copy of the template.tex file with the lab name:
 cp templates/lab_template.tex "$prefix/lab_$1.tex"
